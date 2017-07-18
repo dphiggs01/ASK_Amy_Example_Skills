cdef281c28959b9fb4a8d4580069f6da661dfdd3
Dan Higgins <daniel.higgins@yahoo.com>
2017-07-18 11:53:40 -0400

    updates session skill

--- alexa_session_skill/alexa_session_skill.py
+++ alexa_session_skill/alexa_session_skill.py
@@ -1,4 +1,6 @@
 from ask_amy.state_mgr.stack_dialog_mgr import StackDialogManager
+from ask_amy.state_mgr.stack_dialog_mgr import required_fields
+
 from ask_amy.core.reply import Reply
 import logging
 
@@ -6,24 +8,15 @@ logger = logging.getLogger()
 
 class AlexaSessionSkill(StackDialogManager):
 
+    @required_fields(['Color'])
     def my_color_is_intent(self):
         logger.debug("**************** entering {}.{}".format(self.__class__.__name__, self.intent_name))
-        # 2. See if we have any slots filled
-        self.event.slot_data_to_session_attributes()
-
-        # 3. Do we we have all our required fields
-        need_additional_data = self.required_fields_process(['Color'])
-        if need_additional_data is not None:
-            return need_additional_data
-
+        color = self.request.attributes['Color']
+        self.session.attributes['Color'] = color
         return self.handle_default_intent()
 
+    @required_fields(['Color'])
     def whats_my_color_intent(self):
         logger.debug("**************** entering {}.{}".format(self.__class__.__name__, self.intent_name))
-
-        need_additional_data = self.required_fields_process(['Color'])
-        if need_additional_data is not None:
-            return need_additional_data
-
         return self.handle_default_intent()
 
